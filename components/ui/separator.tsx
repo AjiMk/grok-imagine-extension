151d89ce18fd74179755f83eedde16b0638d87d6
Ajay Kumar M <ajaymeledath007@gmail.com>
2026-04-19 21:07:44 +0530

    Initial commit for chrome extention file changes

--- components/ui/separator.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export function Separator({ className = "", ...props }: React.HTMLAttributes<HTMLHRElement>) {
-  return <hr className={["border-border", className].join(" ")} {...props} />;
-}
