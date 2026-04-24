981acac97026e79b40547fb49cfceb9853bcdb25
Ajay Kumar M <ajaymeledath007@gmail.com>
2026-04-24 16:06:22 +0530

    Fix submit form request submission in content script

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,23 @@ function generate() {
     alert('Please enter a prompt');
     return;
   }
-  console.log('Generating:', { prompt: prompt.value, type: generationType.value, ratio: imageAspectRatio.value, speed: imageSpeed.value });
-  alert('Generation submitted! Check console for details.');
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: 'NOCTURNAL_SUBMIT_PROMPT',
+        generation: {
+          promptText: prompt.value,
+          attachments: [],
+          options: {
+            type: generationType.value,
+            aspectRatio: imageAspectRatio.value,
+            speed: imageSpeed.value
+          }
+        }
+      });
+    }
+  });
 }
 </script>
 
